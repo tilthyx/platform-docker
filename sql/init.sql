3a9a4729797fe95e6ddbc819117b4cddd9c07c62
Lucas Vieira <tilthyx@gmail.com>
2024-11-17 17:58:50 -0300

    Adding more tables into init db

--- sql/init.sql
+++ sql/init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE
     IF NOT EXISTS teams (
         id SERIAL PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
+        name VARCHAR(255) NOT NULL UNIQUE,
         short_name VARCHAR(10) NOT NULL,
         slug VARCHAR(255) NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -9,7 +9,7 @@ CREATE TABLE
 
 CREATE TABLE
     IF NOT EXISTS players (
-        player_name VARCHAR(100),
+        player_name VARCHAR(100) UNIQUE,
         team_abbreviation VARCHAR(10),
         age INT,
         player_height FLOAT,
@@ -31,4 +31,40 @@ CREATE TABLE
         ast_pct FLOAT,
         season VARCHAR(20),
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+CREATE TABLE
+    IF NOT EXISTS players_draft (
+        id SERIAL PRIMARY KEY,
+        player_name VARCHAR(100) UNIQUE,
+        season INT,
+        round_number INT,
+        round_pick INT,
+        overall_pick INT,
+        draft_type VARCHAR(50),
+        team_id INT,
+        team_city VARCHAR(100),
+        team_name VARCHAR(100),
+        team_abbreviation VARCHAR(10),
+        organization VARCHAR(100),
+        organization_type VARCHAR(50),
+        player_profile_flag INT
+    );
+
+CREATE TABLE
+    IF NOT EXISTS team_details (
+        id SERIAL PRIMARY KEY,
+        abbreviation VARCHAR(10) UNIQUE NOT NULL,
+        nickname VARCHAR(100),
+        yearfounded INT,
+        city VARCHAR(100),
+        arena VARCHAR(100),
+        arenacapacity INT,
+        owner VARCHAR(100),
+        generalmanager VARCHAR(100),
+        headcoach VARCHAR(100),
+        dleagueaffiliation VARCHAR(100),
+        facebook VARCHAR(200),
+        instagram VARCHAR(200),
+        twitter VARCHAR(200)
     );
